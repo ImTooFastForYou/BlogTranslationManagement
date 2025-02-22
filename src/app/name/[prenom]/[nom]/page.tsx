27c814baddcd02afb2f70b0b5d20560e4375ea4d
Allan Villars <allan.villars@telecom-paris.fr>
2025-02-22 22:28:32 +0100

    Add age retrieval functionality to name page

--- src/app/name/[prenom]/[nom]/page.tsx
+++ src/app/name/[prenom]/[nom]/page.tsx
@@ -1,38 +1,73 @@
-//import { InputAgeForm } from "@/components/input-form-age";
+import { PrismaClient } from "@prisma/client";
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
-export default function Page({
+const prisma = new PrismaClient();
+
+async function findPersonAge(prenom: string, nom: string) {
+  const user = await prisma.name.findFirst({
+    where: { prenom, nom },
+  });
+
+  if (user) {
+    return user.age;
+  } else {
+    return false;
+  }
+}
+
+export default async function Page({
   params,
 }: {
   params: { prenom: string; nom: string };
 }) {
   console.log(params);
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Enter the age</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form action="/api/name" method="post">
-          <input type="hidden" name="prenom" value={params.prenom} />
-          <input type="hidden" name="nom" value={params.nom} />
-          <label>
-            Age:
-            <input type="text" name="age" />
-          </label>
-          <button type="submit">Submit</button>
-        </form>
-        <div>
-          <Link
-            className={buttonVariants({ size: "lg", variant: "outline" })}
-            href={"/"}
-          >
-            Retour
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-  );
+  const age = await findPersonAge(params.prenom, params.nom);
+  if (age == false) {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>Enter the age</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form action="/api/name" method="post">
+            <input type="hidden" name="prenom" value={params.prenom} />
+            <input type="hidden" name="nom" value={params.nom} />
+            <label>
+              Age:
+              <input type="text" name="age" />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+          <div>
+            <Link
+              className={buttonVariants({ size: "lg", variant: "outline" })}
+              href={"/"}
+            >
+              Retour
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  } else {
+    return (
+      <Card>
+        <CardHeader>
+          <CardTitle>The age is {age}</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <Link
+              className={buttonVariants({ size: "lg", variant: "outline" })}
+              href={"/"}
+            >
+              Retour
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
 }

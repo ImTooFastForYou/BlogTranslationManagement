86f6701d9dd33c538cbac6da4a5bfc52b97ab8e3
Allan Villars <allan.villars@telecom-paris.fr>
2025-02-23 00:22:44 +0100

    Update Tailwind CSS to version 3.4.17; refactor layout and page components to include mode toggle and improve structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,39 @@
+import "./globals.css";
+import { Geist_Mono } from "next/font/google";
+import { Metadata } from "next";
+import { ModeToggle } from "@/components/mode-toggle";
 import { ThemeProvider } from "@/components/theme-provider";
 
-interface RootLayoutProps {
-  children: React.ReactNode;
-}
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export const metadata: Metadata = {
+  title: "Root Layout",
+  description: "Root layout for the app",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
+    <html lang="en" suppressHydrationWarning>
+      <body className={geistMono.variable + "antialiased"}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="p-4 flex justify-end">
+            <ModeToggle />
+          </header>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { ModeToggle } from "@/components/mode-toggle";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { InputNameForm } from "@/components/input-form-front-page";
 
 export default function Page() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div style={{ textAlign: "right", marginTop: "1rem" }}>
-        <ModeToggle />
-      </div>
       <Card>
         <CardHeader>
           <CardTitle>Entrez un prénom et un nom</CardTitle>

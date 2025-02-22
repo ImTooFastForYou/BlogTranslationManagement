e0f202646450765a54bddb4c8b2d25f4e70914e1
Allan Villars <allan.villars@telecom-paris.fr>
2025-02-22 22:38:28 +0100

    Implement age-based user retrieval in the age page

--- src/app/age/[age]/page.tsx
+++ src/app/age/[age]/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <div className=""> Bonsoir ! </div>;
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function findByAge(age: string) {
+  const user = await prisma.name.findMany({
+    where: { age },
+  });
+  if (user) {
+    return user;
+  } else {
+    return false;
+  }
+}
+
+export default async function Page({ params }: { params: { age: string } }) {
+  console.log(params);
+
+  const user = await findByAge(params.age);
+  if (user == false) {
+    return <div className=""> {user} </div>;
+  }
+  return <div className="text-4xl"> No user of that age </div>;
 }
